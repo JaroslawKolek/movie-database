7cc3c9e5ce8b4e9a6e53b2ba3e16d1add594010b
Jaroslaw Kolek <jkolek283@gmail.com>
2019-01-10 17:33:12 +0100

    Basic Test for Models from Movies App

---
+++ backend/movies/tests/movie_test_object_creator.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+from ..models import Movie, Production, Director
+
+
+class MovieTestObjectCreator:
+    """Class to quick-create objects from current app for testing"""
+    # TODO:  Add mommy.recipe
+
+    def create_movie(self, title, language="PL", runtime="20 min",
+                     release_date=date.today(), poster_url="asd.com",
+                     director=None, production=None):
+        return Movie.objects.create(
+            title=title,
+            language=language,
+            runtime=runtime,
+            release_date=release_date,
+            poster_url=poster_url,
+            director=director,
+            production=production
+        )
+
+    def create_director(self, name="Jarek Jarkowski"):
+        return Director.objects.create(name=name)
+
+    def create_production(self, name="Jarkowski Pictures"):
+        return Production.objects.create(name=name)
