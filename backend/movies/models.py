072c5023667c27e4da652146ad26e679f99e9578
Jaroslaw Kolek <jkolek283@gmail.com>
2019-01-10 10:57:23 +0100

    Added new App - Movies | Added Base Model

---
+++ backend/movies/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Production(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=256)
+    language = models.CharField(max_length=120, blank=True)
+    runtime = models.CharField(max_length=50, blank=True)
+    release_date = models.DateField()
+    director = models.ForeignKey(Director, null=True, on_delete=models.SET_NULL)
+    production = models.ForeignKey(Production, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
