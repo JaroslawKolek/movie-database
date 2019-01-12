0c363388d4474f7ce1b9f34cc0e7f1523d10a397
Jaroslaw Kolek <jkolek283@gmail.com>
2019-01-12 21:53:06 +0100

    Fixed Migrations and MovieListView

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 
 from .services.omdb_api_service import OMDbService
 from .serializers.movies_serializer import MovieSerializer
+from .models import Movie, Production
 
 
 class MoviesListView(APIView):
@@ -13,65 +14,23 @@ class MoviesListView(APIView):
     # permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        # omdb_service = OMDbService()
-        #
-        # title = request.GET.get('title')
-        # page = request.GET.get('page', 1)
-        #
-        # if not title:
-        #     Response("Title param is mandatory!", status=400)
-        #
-        # return Response({
-        #     'movies': omdb_service.get_movies_by_title_search(title, page)
-        # })
+        omdb_service = OMDbService()
+        title = request.GET.get('title', '')
+        page = request.GET.get('page', 1)
+
         response = {
-            'movies': [{
-                "Title": "X-Men",
-                "Year": "2000",
-                "Rated": "PG-13",
-                "Released": "14 Jul 2000",
-                "Runtime": "104 min",
-                "Genre": "Action, Adventure, Sci-Fi",
-                "Director": "Bryan Singer",
-                "Writer": "Tom DeSanto (story), Bryan Singer (story), David Hayter (screenplay)",
-                "Actors": "Hugh Jackman, Patrick Stewart, Ian McKellen, Famke Janssen",
-                "Plot": "Two mutants come to a private academy for their kind whose resident superhero team must oppose a terrorist organization with similar powers.",
-                "Language": "English",
-                "Country": "USA",
-                "Awards": "13 wins & 26 nominations.",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BZmJkOGY4YjYtM2FmYy00MTIyLTg2YTUtMzJiNjljMWM5MGUwXkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg",
-                "Ratings": [
-                    {
-                        "Source": "Internet Movie Database",
-                        "Value": "7.4/10"
-                    },
-                    {
-                        "Source": "Rotten Tomatoes",
-                        "Value": "81%"
-                    },
-                    {
-                        "Source": "Metacritic",
-                        "Value": "64/100"
-                    }
-                ],
-                "Metascore": "64",
-                "imdbRating": "7.4",
-                "imdbVotes": "520,792",
-                "imdbID": "tt0120903",
-                "Type": "movie",
-                "DVD": "21 Nov 2000",
-                "BoxOffice": "$156,164,829",
-                "Production": "20th Century Fox",
-                "Website": "http://www.x-men-the-movie.com",
-                "Response": "True"
-            }]
+            'movies': omdb_service.get_movies_by_title_search(title, page)
         }
+
         for movie in response.get('movies', []):
-            pass
-            # with transaction.atomic():
-            #     # get or create by idF
-            #     # do update
-            # # TODO get or create
+            production, _ = Production.objects.get_or_create(name=movie.get('Production'))
+            movie_obj, _ = Movie.objects.update_or_create(
+                imdb_id=movie.get('imdbID'),
+                defaults={
+                    'title': movie.get('Title'),
+                    'production': production
+                }
+            )
         return Response(data=response)
 
 
