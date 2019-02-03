a222a815f9a07f16dbdd8b2a93aee7be0290a2b9
Jaroslaw Kolek <jkolek283@gmail.com>
2019-02-03 17:41:02 +0100

    Moved Login and Logout components

--- frontend/src/components/LogoutPage/LogoutPage.js
+++ frontend/src/components/LogoutPage/LogoutPage.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap';
 
-import LogoutComponent from '../../common/LogoutComponent/LogoutComponent';
+import { post, getToken } from '../../utils/api';
 
 class LogoutPage extends React.Component {
+    state = {
+        responseStatus: undefined
+    };
+
+    componentDidMount() {
+        this.logout();
+    }
+
+    logout = () => {
+        if(getToken() !== undefined){
+            post('/users/logout/', {}).then(response => {
+                let status = response.status;
+                this.setState({responseStatus: status})
+                if(status === 200){
+                    localStorage.setItem('token', '');
+                    localStorage.setItem('username', '');
+                }
+            })
+        }     
+    }
+
+    renderAlert = () => {
+        if(this.state.responseStatus === 200) {
+            return <Alert bsStyle="success"> You Have Successfully Logged Out Of Your Account. </Alert>
+        } else if (this.state.responseStatus === 401) {
+            return <Alert bsStyle="danger"> You are logged out. Please, log in to use search section</Alert>
+        }
+        return;
+    }
+
     render() {
         return (
             <div>
-                <LogoutComponent />
+                { this.renderAlert() }
             </div>
         )
     }
